6459475da72e88f54f29ac3f57fa70b13cefb66f
Mike <q23362352@yahoo.com.tw>
2019-09-16 21:03:34 +0800

    second read

--- read.py
+++ read.py
@@ -7,7 +7,12 @@ with open('reviews.txt', 'r' ) as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
-print('----------------------------------------------')
-print(data[1])
+print('檔案完成')
+
+sum_len = 0
+
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+
+print('平均是', sum_len/len(data))
